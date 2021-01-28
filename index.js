c0c2b9781a77ad3cea44e56270cba9b1b8ff54d0
Cavliuc <cavliuc.sh@inbox.ru>
2021-01-28 15:25:48 +0200

    filter card

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-(function() { //скрытие базы от легкого доступа через консоль( в конце закрытие)
+const btns = document.querySelectorAll('.filter-btns_item');
+const select = document.getElementById('filter-options');
+const images = document.querySelectorAll('.filter-content img');
 
-    const tabs = document.querySelectorAll(".tab-head_item");
-    const contents = document.querySelectorAll(".tab-content_item");
-    
-    tabs.forEach((tab, index) => {
-        tab.onclick = () => {
-            changeTab(index);
-        };
-    });
-    
-    function changeTab(index) {
-       setActiveClass(tabs, index);
-       setActiveClass(contents, index);
-    }
-    
-    function setActiveClass (arr, index) {
-        for(let el of arr) {
-            el.classList.remove("active");
+for(let btn of btns) {
+    btn.onclick = function() {
+        filterImage(this.dataset.filter);
+    };
+}
+
+function filterImage(sel) {
+    for(let img of images) {
+        if(!img.classList.contains(sel)) {
+            img.style.display= "none";
+        } else {
+            img.style.display="block";
         }
-        arr[index].classList.add("active");
-    
     }
-}) ();
 
+    for (let btn of btns) {
+        if (btn.dataset.filter === sel) {
+            btn.classList.add('active');
+        }else {
+            btn.classList.remove("active");
+        }
+    }
+    select.value = sel;
+}
+
+select.addEventListener('change', () =>{
+    filterImage(select.value);
+});
